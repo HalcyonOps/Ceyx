96b79fa5fbbe4af68a683038858a0f14aaf0c163
Ross Tomsic <ross.tomsic@gmail.com>
2024-09-30 15:54:28 -0400

    refined aws code

--- src/aws/main.tf
+++ src/aws/main.tf
@@ -4,21 +4,34 @@ provider "aws" {
 
 module "aws_compute" {
   source = "../../modules/compute/aws"
-  name   = var.name
-  region = var.region
-  tags   = var.tags
+
+  # Required arguments
+  name               = var.name
+  ami_id             = var.ami_id
+  instance_type      = var.instance_type
+  region             = var.region
+  subnet_id          = var.subnet_id
+  security_group_ids = var.security_group_ids
+  tags               = var.compute_tags
 }
 
 module "aws_network" {
   source = "../../modules/network/aws"
-  name   = var.name
-  region = var.region
-  tags   = var.tags
+
+  # Required arguments
+  subnet_cidr_block = var.subnet_cidr_block
+  availability_zone = var.availability_zone
+  cidr_block        = var.cidr_block
+  region            = var.region
 }
+
 
 module "aws_storage" {
   source = "../../modules/storage/aws"
-  name   = var.name
-  region = var.region
-  tags   = var.tags
+
+  # Required arguments
+  region            = var.region
+  bucket_name       = var.bucket_name
+  tags              = var.storage_tags
+  versioning_status = var.versioning_status
 }
--- src/aws/variables.tf
+++ src/aws/variables.tf
@@ -1,15 +1,74 @@
-variable "name" {
-  description = "The name of the project."
+variable "ami_id" {
+  description = "The AMI ID to use for the EC2 instance."
   type        = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance."
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "region" {
-  description = "The region where resources will be deployed."
+  description = "The AWS region to deploy the instance."
   type        = string
 }
 
+variable "subnet_id" {
+  description = "The subnet ID to deploy the instance."
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "A list of security group IDs to assign to the instance."
+  type        = list(string)
+}
+
 variable "tags" {
-  description = "Common tags to be applied to all resources."
+  description = "A map of tags to assign to the resource."
   type        = map(string)
   default     = {}
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket."
+  type        = string
+}
+
+variable "name" {
+  description = "The name of the EC2 instance."
+  type        = string
+}
+
+variable "subnet_cidr_block" {
+  description = "The CIDR block for the subnet."
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "The availability zone for the subnet."
+  type        = string
+}
+
+variable "cidr_block" {
+  description = "The CIDR block for the VPC."
+  type        = string
+}
+
+variable "compute_tags" {
+  description = "A map of tags to assign to the EC2 instance."
+  type        = map(string)
+  default     = {}
+}
+
+variable "storage_tags" {
+  description = "A map of tags to assign to the S3 bucket."
+  type        = map(string)
+  default     = {}
+}
+
+variable "versioning_status" {
+  description = "The status of versioning for the S3 bucket (Enabled or Suspended)."
+  type        = string
+  default     = "Enabled" # or "Suspended"
 }
